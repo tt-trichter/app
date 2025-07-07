2af9c444b2e7533ef5b2f19946007952530ae522
Simon <simon21.blum@gmail.com>
2025-07-07 11:10:41 +0200

    ci: build image on tag push

--- util/create-run-dev.sh
+++ util/create-run-dev.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-rate=16
-duration=2.875
-volume=0.8
+random_float() {
+  awk -v min="$1" -v max="$2" 'BEGIN{srand(); print min + rand() * (max - min)}'
+}
+
+rate=$(random_float 4 12 | awk '{printf "%.2f", $0}')
+duration=$(random_float 2 10 | awk '{printf "%.2f", $0}')
+volume=$(echo "scale=2; $rate * $duration / 60" | bc)
 
 auth="Basic dHJpY2h0ZXI6c3VwZXItc2FmZS1wYXNzd29yZA=="
 
